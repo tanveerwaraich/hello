d71fc2efb50ab9fbf3faa8c5d5f5b118c6bd87b3
tanveerwaraich <i120322@nu.edu.pk>
2016-03-10 14:46:51 +0500

    first commit

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/tanveerwaraich/greetings"
+)
+
+func main() {
+	//	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	fmt.Printf(greetings.Greetingmessage("Tanveer"))
+}
